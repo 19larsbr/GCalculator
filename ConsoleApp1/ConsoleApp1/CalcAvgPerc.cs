ef70746eab708755934d0fef0d098700528899e8
Bri <Bri@DESKTOP-979S06F>
2017-11-06 09:43:47 -0500

    Finished code

---
+++ ConsoleApp1/ConsoleApp1/CalcAvgPerc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class CalcAvgPerc
+    {
+        public CalcAvgPerc()
+        {
+        }
+
+        internal void Rectangle()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Circle()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Square()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Triangle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
